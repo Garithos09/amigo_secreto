70e0e9b110c6bce92dd55e4d34f661a1f3045ccf
garithos <garithosbot@gmail.com>
2025-02-15 16:39:22 -0500

    amigo secreto challenge

---
+++ app.js
@@ -0,0 +1,13 @@
+// El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+// AGREGAR NOMBRES
+
+// VALIDAR ENTRADA
+
+// VISUALIZAR LOS NOMBRES EN UNA LISTA
+
+// SORTEAR Y MOSTRAR EL NOMBRE DEL AMIGO SECRETO
+
+function agregarAmigo(){
+    let amigos = document.getElementById("amigo").value;
+
+}
